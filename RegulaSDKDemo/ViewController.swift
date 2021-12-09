ba2a5e23a7286f1457c2c9c58d1af0e67999ca31
Yuşa Sarısoy <yusasarisoy@icloud.com>
2021-12-09 19:12:50 +0300

    Initial Commit

---
+++ RegulaSDKDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegulaSDKDemo
+//
+//  Created by Yuşa on 9.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
